df18f57e5c3238773c08bcdf30b19088a75e6da3
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2022-10-13 15:25:02 +0200

    filters added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatButtonModule } from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { DecksComponent } from './components/decks/decks.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// Angular Material imports
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -28,6 +32,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     MatGridListModule,
     MatButtonModule,
     MatButtonToggleModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatExpansionModule,
     NgbModule
   ],
   providers: [],
--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -3,14 +3,113 @@ import { Observable } from 'rxjs';
 import { Card } from 'src/app/models/Card';
 import { CardsService } from 'src/app/services/cards.service';
 
+interface Attack {
+  value: string;
+  viewValue: string;
+}
+interface Health {
+  value: string;
+  viewValue: string;
+}
+interface Rarity {
+  value: string;
+  viewValue: string;
+}
+interface CardType {
+  value: string;
+  viewValue: string;
+}
+interface Tribe {
+  value: string;
+  viewValue: string;
+}
+interface SpellSchool {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
+
+
 export class CardsComponent implements OnInit {
 
   cards$: Observable<Card[]> = new Observable;
+  attackValue = null;
+  healthValue = null;
+  rarityValue = null;
+  cardTypeValue = null;
+  tribeValue = null;
+  spellSchoolValue = null;
+
+  attack: Attack[] = [
+    {value: '0', viewValue: '0'},
+    {value: '1', viewValue: '1'},
+    {value: '2', viewValue: '2'},
+    {value: '3', viewValue: '3'},
+    {value: '4', viewValue: '4'},
+    {value: '5', viewValue: '5'},
+    {value: '6', viewValue: '6'},
+    {value: '7', viewValue: '7'},
+    {value: '8', viewValue: '8'},
+    {value: '9', viewValue: '9'},
+    {value: '10', viewValue: '10+'},
+  ];
+
+  health: Health[] = [
+    {value: '1', viewValue: '1'},
+    {value: '2', viewValue: '2'},
+    {value: '3', viewValue: '3'},
+    {value: '4', viewValue: '4'},
+    {value: '5', viewValue: '5'},
+    {value: '6', viewValue: '6'},
+    {value: '7', viewValue: '7'},
+    {value: '8', viewValue: '8'},
+    {value: '9', viewValue: '9'},
+    {value: '10', viewValue: '10+'},
+  ];
+
+  rarity: Rarity[] = [
+    {value: 'Free', viewValue: 'Free'},
+    {value: 'Common', viewValue: 'Common'},
+    {value: 'Rare', viewValue: 'Rare'},
+    {value: 'Epic', viewValue: 'Epic'},
+    {value: 'Legendary', viewValue: 'Legendary'}
+  ];
+
+  cardType: CardType[] = [
+    {value: 'heroCard', viewValue: 'Hero'},
+    {value: 'minionCard', viewValue: 'Minion'},
+    {value: 'spellCard', viewValue: 'Spell'},
+    {value: 'weaponCard', viewValue: 'Weapon'}
+  ];
+
+  tribe: Tribe[] = [
+    {value: 'All', viewValue: 'All'},
+    {value: 'Beast', viewValue: 'Beast'},
+    {value: 'Demon', viewValue: 'Demon'},
+    {value: 'Dragon', viewValue: 'Dragon'},
+    {value: 'Elemental', viewValue: 'Elemental'},
+    {value: 'Mech', viewValue: 'Mech'},
+    {value: 'Murloc', viewValue: 'Murloc'},
+    {value: 'Naga', viewValue: 'Naga'},
+    {value: 'Pirate', viewValue: 'Pirate'},
+    {value: 'Quilboar', viewValue: 'Quilboar'},
+    {value: 'Totem', viewValue: 'Totem'}
+  ];
+
+  spellSchool: SpellSchool[] = [
+    {value: 'Arcane', viewValue: 'Arcane'},
+    {value: 'Fel', viewValue: 'Fel'},
+    {value: 'Fire', viewValue: 'Fire'},
+    {value: 'Frost', viewValue: 'Frost'},
+    {value: 'Holy', viewValue: 'Holy'},
+    {value: 'Nature', viewValue: 'Nature'},
+    {value: 'Shadow', viewValue: 'Shadow'}
+  ];
 
   constructor(private cardsService: CardsService) { }
 
