396b2f829c5cdcd36011df7fb34bf791c01a14e3
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2022-11-15 15:28:15 +0100

    styling + cardsGrid responsive

--- src/app/services/admin-guard.service.ts
+++ src/app/services/admin-guard.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 
 @Injectable()
 export class AdminCardGuardService implements CanActivate{
